ef5c04d41d35a6930ef3fe2dbda040ce402c9452
Henrique Kuwai <rikezenho@gmail.com>
2018-10-23 18:00:23 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Add Swagger Link to Eureka
+
+If matches 'Eureka' in `document.title`, it will iterate every item in Eureka's list and replace with a text + swagger link.
+
+---
+
+## Installing
+- Open `chrome://extensions` in your Google Chrome
+- Activate 'Developer mode'
+- Click on 'Load unpacked extension'
+- Select the `/ext` folder inside extensions' files
---
+++ ext/src/inject/eureka.js
@@ -0,0 +1,18 @@
+const addSwaggerLinksToEureka = () => {
+  const instances = [...document.querySelectorAll('#instances tr a:not(.swagger-link-added)')];
+  instances.map(el => {
+    const theLink = el.href;
+    const splitted = theLink.split('/');
+
+    el.outerHTML = `
+    <span>
+      <a href='${el.href}' target='_blank'>${el.innerText}</a>
+      <a href='${`${splitted[0]}//${splitted[2]}/swagger-ui.html`}' style='color:#000;'>[swagger]</a>
+    </span>
+    `
+  })
+}
+
+if (document.title === 'Eureka') {
+  addSwaggerLinksToEureka();
+}
